830011f958eb9a6bbdef3ffbe9f47a83823e3063
Tim Pietrusky <timpietrusky@googlemail.com>
2013-07-15 14:22:39 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+emmmmmm.com
+================
+
+px suck - love (r)emmmmmm
+
+-------
+
+This is an old religion based on em.  Only one rule: **We hate px and love (r)em. **
+
+-------
+
+[emmmmmm.com](http://emmmmmm.com)
---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>emmmmmm</title>
+
+    <meta charset="utf-8">
+    <meta name="author" content="Tim Pietrusky,4ae9b8">
+    <meta name="robots" content="index,follow">
+    <meta name="revisit-after" content="1 days">
+    <meta name="description" content="px suck - love (r)emmmmmm">
+
+    <meta property="og:title" content="emmmmmm">
+    <meta property="og:description" content="px suck - love (r)emmmmmm">
+    <meta property="og:image" content="http://emmmmmm.com/img/emmmmmm_200.jpg">
+    <meta property="og:url" content="http://emmmmmm.com">
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="stylesheet" href="css/emmmmmm.css">
+    <link rel="shortcut icon" href="img/emmmmmm.ico" type="image/x-icon">
+</head>
+
+<body>
+
+<div class="wrapper">
+  <h2>px suck</h2>
+  <h1><a href="http://emmmmmm.com">love (r)emmmmmm</a></h1>
+</div>
+   
+</body>
+
+<script src="js/emmmmmm.js"></script>
+
+</html>
